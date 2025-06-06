f381a2e080b48dfa85b2b1c5ead263a666274f57
Abanoub Maged Shawky <abanoubmaged185@gmail.com>
2025-06-06 19:07:56 +0300

    Resolved merge conflict in changePasswordApiServic

--- lib/features/change_password/data/apis/change_password_api_service.dart
+++ lib/features/change_password/data/apis/change_password_api_service.dart
@@ -1,27 +1,27 @@
-// import 'package:dio/dio.dart';
-// import 'package:rommify_app/core/networking/api_networking.dart';
-// import 'package:rommify_app/features/change_password/data/models/change_password_request_body.dart';
-// import 'package:rommify_app/features/change_password/data/models/change_password_response.dart';
+import 'package:dio/dio.dart';
+import 'package:rommify_app/core/networking/api_networking.dart';
+import 'package:rommify_app/features/change_password/data/models/change_password_request_body.dart';
+import 'package:rommify_app/features/change_password/data/models/change_password_response.dart';
 
 
-// class ChangePasswordApiService {
-//   final Dio _dio;
+class ChangePasswordApiService {
+  final Dio _dio;
 
-//   ChangePasswordApiService({required Dio dio}) : _dio = dio;
+  ChangePasswordApiService({required Dio dio}) : _dio = dio;
 
-//   Future<ChangePasswordResponse> changePassword(ChangePasswordRequestModel request) async {
-//     try {
-//       final response = await _dio.post(
-//         ApiConstants.changePasswordUrl,
-//         data: request.toJson(),
-//       );
+  Future<ChangePasswordResponse> changePassword(ChangePasswordRequestModel request) async {
+    try {
+      final response = await _dio.post(
+        ApiConstants.changePasswordUrl,
+        data: request.toJson(),
+      );
       
-//       return ChangePasswordResponse.fromJson(response.data);
-//     } on DioException catch (e) {
-//       if (e.response != null) {
-//         throw Exception(e.response?.data['message'] ?? 'Failed to change password');
-//       }
-//       throw Exception('Something went wrong');
-//     }
-//   }
-// }
+      return ChangePasswordResponse.fromJson(response.data);
+    } on DioException catch (e) {
+      if (e.response != null) {
+        throw Exception(e.response?.data['message'] ?? 'Failed to change password');
+      }
+      throw Exception('Something went wrong');
+    }
+  }
+}
