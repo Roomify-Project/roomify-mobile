9cf76b325bec5732a6b196bd371b8d06619b48dd
antoneos <antoneosphilip3@gmail.com>
2025-06-07 15:29:42 -0700

    change pass

---
+++ local_packages/image_gallery_saver/example/test/widget_test.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+//
+// void main() {
+//   testWidgets('Verify Widgets', (WidgetTester tester) async {
+//     // Build our app and trigger a frame.
+//     await tester.pumpWidget(new RoomifyApp(appRouter: AppRouter(),)());
+//     final Finder flatButtonPass = find.widgetWithText(ElevatedButton, '保存屏幕截图');
+//     expect(flatButtonPass, findsOneWidget);
+//   });
+// }
